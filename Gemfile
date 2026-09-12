c61988e737cfed04766499d02d13a3257473b26f
caiotarso <caiotarso7@gmail.com>
2026-09-12 19:50:41 -0300

    feat: adicionado devise para autenticação no projeto

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem "jbuilder"
 
 gem "json", "< 3.0"
 
+gem "devise"
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-
+  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
