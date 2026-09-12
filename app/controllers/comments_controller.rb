8c12cb3d6053cee4c1941c721fd67e5e1cee5041
caiotarso <caiotarso7@gmail.com>
2026-09-12 19:31:50 -0300

    feat: adicionando funcionalidade de excluir comentários

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: :destroy
 
   def create
     @comment = @post.comments.build(comment_params)
@@ -11,10 +12,23 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to @post, notice: "Comentário excluído com sucesso.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params
