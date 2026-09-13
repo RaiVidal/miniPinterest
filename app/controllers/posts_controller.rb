dfe0189bcc0dba033fc7c764c25c810840522ca3
caiotarso <caiotarso7@gmail.com>
2026-09-13 02:12:29 -0300

    feat(alerts): adicionaod alerts do bootstrap globalmente no sistema + traduzindo mensagens de retorno nos controllers de pots e comentário

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to @post, notice: "Post criado com sucesso." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_content }
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated.", status: :see_other }
+        format.html { redirect_to @post, notice: "Post editado com sucesso.", status: :see_other }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_content }
@@ -59,7 +59,7 @@ class PostsController < ApplicationController
     @post.destroy!
 
     respond_to do |format|
-      format.html { redirect_to posts_path, notice: "Post was successfully destroyed.", status: :see_other }
+      format.html { redirect_to posts_path, notice: "Post deletado com sucesso.", status: :see_other }
       format.json { head :no_content }
     end
   end
